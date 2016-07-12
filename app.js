9d1c9356f277ecceb258d9546dcc00fe967c2849
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-11 21:25:52 -0500

    Why Functions?

---
+++ app.js
@@ -0,0 +1,27 @@
+/******************************************************
+*                JavaScript Functions                 *
+* A JavaScript function is a block of code designed   *
+* to perform a particular task.                       *
+* A JavaScript function is executed when "something"  *
+* invokes it (calls it).                              *
+******************************************************/
+function myFunction(a, b) {
+  return a * b;
+}
+document.getElementById("demo").innerHTML =
+myFunction(4, 3);
+
+/******************************************************
+*             JavaScript Function Syntax              *
+  function name(parameter1, parameter2, parameter3) {
+    code to be executed
+  }
+******************************************************/
+
+/******************************************************
+*                   Why Functions?                    *
+******************************************************/
+function toCelsius(f) {
+  return (5/9) * (f-32);
+}
+document.getElementById("fToC").innerHTML = toCelsius(77);
